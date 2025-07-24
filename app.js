1a48dd017bc7e61e8e4ebcfc8d38cf2687a2f2c7
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-07-24 00:14:07 -0600

    Subiendo este proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+comillas simples o dobles son indiferentes en JS
+usar camellCase
+alert('Hola mundo')
+*/
+let numeroUsuario = prompt("Me indicas un numero por favor"); //ingresar un parametro en chrome
+let numeroSecreto = 6;
+
+console.log(numeroUsuario);
+
+if(numeroUsuario == numeroSecreto){
+    alert('Acertaste el numero');
+}
