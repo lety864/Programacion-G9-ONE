5eaf60f82985d3acfc912a8cdeca02776952af40
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-07-25 23:20:21 -0600

    (update) general, se agrego 2 archivos de texto de ejercicios resueltos

--- app.js
+++ app.js
@@ -1,88 +1,31 @@
-/*
-comillas simples o dobles son indiferentes en JS
-usar camellCase
-alert('Hola mundo')
-*/
+//variables
+let numeroSecreto = 5;
+//se saco la variable debido a que el bucle while esta leyendo la variable antes de declararlo
+let numeroUsuario = 0;
+let intentos=1; 
+let palabraVeces='vez';
 
-//#region Codigo de Ejemplo
-let numeroUsuario = prompt("Me indicas un numero por favor"); //ingresar un parametro en chrome
-let numeroSecreto = 6;
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+    console.log(numeroUsuario);
 
-console.log(numeroUsuario);
-
-if(numeroUsuario == numeroSecreto){
-    alert('Acertaste el numero');
+    if(numeroUsuario == numeroSecreto){
+        //interpolacion con comillas invertidas
+        alert(`¡Acertaste, el numero es: ${numeroUsuario}, Lo hiciste en ${intentos} ${palabraVeces}!`);
+    }else{
+        if(numeroUsuario > numeroSecreto){
+            alert("El numero es menor");
+        }else{
+            alert("El numero es mayor");
+        }
+        //incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+    }
 }
-//#endregion
 
-/*
-1 - Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
-*/
 
-alert("¡Bienvenida y bienvenido a nuestro sitio web!");
 
-/*
-2 - Declara una variable llamada nombre y asígnale el valor "Lua".
-*/
 
-let nombre = "Lua";
-console.log(nombre);
 
-/*
-3 - Crea una variable llamada edad y asígnale el valor 25.
-*/
 
-let edad = 25;
-console.log(edad);
-
-/*
-4 - Establece una variable numeroDeVentas y asígnale el valor 50.
-*/
-
-let numeroDeVentas = 50;
-console.log(numeroDeVentas);
-
-/* 
-5 - Establece una variable saldoDisponible y asígnale el valor 1000.
-*/
-
-let saldoDisponible = 1000;
-console.log(saldoDisponible);
-
-/*
-6 - Muestra una alerta con el texto "¡Error! Completa todos los campos".
-*/
-
-alert("¡Error! Completa todos los campos");
-
-/*
-7 - Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". 
-Ahora muestra una alerta con el valor de la variable mensajeDeError .
-*/
-
-let mensajeDeError = "¡Error! Completa todos los campos";
-alert(mensajeDeError);
-
-/*
-8- Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
-*/
-
-let nombre2 = prompt("Cual es tu nombre?");
-console.log(nombre2);
-
-/*
-9 - Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
-*/
-
-let edad2 = prompt("Ingresa tu edad");
-console.log(edad2);
-
-/*
-10 - Ahora, si la edad es mayor o igual a 18, 
-muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
-Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección
-*/
-
-if(edad2 >= 18){
-    alert('"¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección');
-}
