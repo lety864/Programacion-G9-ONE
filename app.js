3a8940f393807ece4a2c66c54db2ce59c9e83399
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-07-29 20:24:44 -0600

    (update) se agrego un archivo con los ejercicios resueltos y programa terminado

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 //variables
-let numeroSecreto = 5;
+let maximoValor = 25;
+let numeroSecreto = Math.floor(Math.random()*maximoValor) + 1;
 //se saco la variable debido a que el bucle while esta leyendo la variable antes de declararlo
 let numeroUsuario = 0;
 let intentos=1; 
-let palabraVeces='vez';
+//let palabraVeces='vez';
+let maximoIntentos = 3;
 
 while(numeroUsuario != numeroSecreto){
-    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${maximoValor} por favor:`));
     console.log(numeroUsuario);
 
     if(numeroUsuario == numeroSecreto){
         //interpolacion con comillas invertidas
-        alert(`¡Acertaste, el numero es: ${numeroUsuario}, Lo hiciste en ${intentos} ${palabraVeces}!`);
+        alert(`¡Acertaste, el numero es: ${numeroUsuario}, Lo hiciste en ${intentos} ${intentos == 1 ? 'Vez' : 'Veces'}!`);
     }else{
         if(numeroUsuario > numeroSecreto){
             alert("El numero es menor");
@@ -19,12 +21,16 @@ while(numeroUsuario != numeroSecreto){
             alert("El numero es mayor");
         }
         //incrementamos el contador cuando no acierta
-        intentos = intentos + 1;
-        palabraVeces = 'veces';
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+        //palabraVeces = 'veces';
+        if(intentos > maximoIntentos){
+            alert(`Llegaste al numero maximo de ${maximoIntentos} intentos`);
+            break;
+        }
     }
 }
-
-
 
 
 
